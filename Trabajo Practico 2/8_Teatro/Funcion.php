4c620dc06bd381dc3fb144ccf5ad8c9927085215
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-05-27 18:34:30 -0300

    Creacion: Nueva class Teatro

---
+++ Trabajo Practico 2/8_Teatro/Funcion.php
@@ -0,0 +1,54 @@
+<?php
+# las funciones sean un objeto que tenga las
+# variables nombre, horario de inicio, duración de la obra y precio
+class Funcion{
+    // Zona de atributos
+    private $nombre;
+    private $horaInicio;
+    private $duracionObra;
+    private $precio;
+    // Zona de metodos
+    // Metodo constructor (__construct)
+    public function __construct($nombreIng,$horaInicioIng,$duracionObraIng,$precioIng) {
+        $this->nombre = $nombreIng;
+        $this->horaInicio = $horaInicioIng;
+        $this->duracionObra = $duracionObraIng;
+        $this->precio = $precioIng;
+    }
+    // Metodos de modificacion (Get's)
+    public function getNombre() {
+        return $this->nombre;
+    }
+    public function getHoraInicio() {
+        return $this->horaInicio;
+    }
+    public function getDuracionObra() {
+        return $this->duracionObra;
+    }
+    public function getPrecio() {
+        return $this->precio;
+    }
+    // Metodos de modificacion (Set's)
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+    public function setHoraInicio($horaInicio) {
+        $this->horaInicio = $horaInicio;
+    }
+    public function setDuracionObra($duracionObra) {
+        $this->duracionObra = $duracionObra;
+    }
+    public function setPrecio($precio) {
+        $this->precio = $precio;
+    }
+    // Metodo de caracteres (__toString)
+    public function __toString()
+    {
+        $cadena = "\nNombre del teatro: " . $this->getNombre() . "\n";
+        $cadena .= "Hora de inicio de la obra: " . $this->getHoraInicio() . "\n";
+        $cadena .= "Duracion de la obra: " . $this->getDuracionObra() . "\n";
+        $cadena .= "Precio de la obra: " . $this->getPrecio() . "\n";
+        return $cadena;
+    }
+}
+?>
